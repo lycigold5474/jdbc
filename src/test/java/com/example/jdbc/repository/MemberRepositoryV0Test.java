350360cd00cb57d487529d22c29f94bbd21ff075
andy <lycigold@gmail.com>
2022-08-16 22:33:11 +0900

    1007. 회원수정 및 테스트

--- src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,7 +2,6 @@ package com.example.jdbc.repository;
 
 import com.example.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -24,6 +23,11 @@ class MemberRepositoryV0Test {
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
     }
 
 
