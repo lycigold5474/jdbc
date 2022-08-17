82d9c6ddda2f158c8e085c2fa601f9a4cc2d3be2
andy <lycigold@gmail.com>
2022-08-17 21:44:11 +0900

    1010. 데이터소스 커넥션 풀 추가 테스트

--- src/test/java/com/example/jdbc/connection/ConnectionTest.java
+++ src/test/java/com/example/jdbc/connection/ConnectionTest.java
@@ -1,9 +1,11 @@
 package com.example.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 import static com.example.jdbc.connection.ConnectionConst.*;
@@ -27,7 +29,21 @@ class ConnectionTest {
         useDataSource(dataSource);
     }
 
-    private void useDataSource(DriverManagerDataSource dataSource) throws SQLException {
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); //커넥션 풀에서 커넥션 생성 시간 대기
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());;
