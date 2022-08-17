e885e66e9311f68abde59dad509b36aed6709ed2
andy <lycigold@gmail.com>
2022-08-17 21:20:55 +0900

    1008. DriverManager를 통한 커넥션획득 테스트

---
+++ src/test/java/com/example/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,23 @@
+package com.example.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.example.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+}
